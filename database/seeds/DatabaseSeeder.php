b5e7ba088d2483eb9805132995d5fd2338d72944
Jame <jame191036@gmail.com>
2020-10-05 22:42:49 +0700

    test seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'title' => Str::random(10),
+            'description' => Str::random(1000),
+            'content' => Str::random(10),
+            'image' => Str::random(10).'.jpg',
+        ]);
     }
 }
