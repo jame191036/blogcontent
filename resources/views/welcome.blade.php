83bada85a49549fe961ac99c6c28ee9d2ff92b66
Sornrak <evolutionallstar@gmail.com>
2020-10-06 16:38:35 +0700

    edit welcome pang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel dfsdfs dfsdfaefaeefadgit
+                    Laravel
                 </div>
 
                 <div class="links">
